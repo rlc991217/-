fb80d4e33af7639e6f7f5b29cb886cae7b9eaa03
Your Name <1298702759@qq.com>
2020-12-03 09:03:01 +0800

    first commit

---
+++ src/views/dianyin.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <Header></Header>
+        <Movie></Movie>
+        <!-- <City></City> -->
+        <router-view/>
+        <TabBar></TabBar>
+    </div>
+</template>
+<script>
+import City from '@/components/City.vue'
+import Header from '@/components/Header.vue'
+import Movie from '@/components/Movie.vue'
+import TabBar from '@/components/TabBar.vue'
+export default {
+    components:{
+        Header,
+        Movie,
+        City,
+        TabBar
+    }
+}
+</script>
+<style>
+    *{
+        padding: 0;
+        margin: 0;
+    }
+</style>
---
+++ src/views/yingyuan.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+         <Header></Header>
+         <Cinema></Cinema>
+         <CiList></CiList>
+        <TabBar></TabBar>
+    </div>
+</template>
+<script>
+import Header from '@/components/Header.vue'
+import CiList from '@/components/CiList.vue'
+import Cinema from '@/components/Cinema.vue'
+import TabBar from '@/components/TabBar.vue'
+export default {
+    components:{
+        Header,
+        Cinema,
+        CiList,
+        TabBar
+    }
+}
+</script>
+<style>
+    *{
+        padding: 0;
+        margin: 0;
+    }
+</style>
