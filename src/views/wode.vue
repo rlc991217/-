7c5c1851682f98bd9ee78c96639d781322ed4306
Your Name <1298702759@qq.com>
2020-12-03 08:37:33 +0800

    first commit

---
+++ src/views/wode.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>  
+       <Header></Header>
+       <Mine></Mine>
+       <TabBar></TabBar>
+    </div>
+</template>
+ <script>
+import Header from '@/components/Header.vue'
+import Mine from '@/components/Mine.vue'
+import TabBar from '@/components/TabBar.vue'
+
+export default {
+    components:{
+        Header,
+        Mine,
+        TabBar
+    }
+}
+</script>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+    }
+</style>
